67b9c4a3282ea17f99c488ecbb8450d0171be2ce
Akhi <114737969+Akhila143L@users.noreply.github.com>
2022-10-06 16:23:22 +0530

    Add files via upload

---
+++ SearchAccountMain.java
@@ -0,0 +1,23 @@
+package com.infinite.bankproject;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class SearchAccountMain {
+public static void main(String[] args) {
+	int accountNo;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter AccountNo   ");
+	accountNo = sc.nextInt();
+	BankDAO dao = new BankDAO();
+	try {
+		Bank bank = dao.searchAccount(accountNo);
+		System.out.println(bank);
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	
+}
+}
